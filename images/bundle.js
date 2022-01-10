a5e0baabf80da898e509ef6f8970549b6ad815f2
parthdabhi42 <89513550+parthdabhi42@users.noreply.github.com>
2022-01-10 15:44:56 +0530

    error change

--- images/bundle.js
+++ images/bundle.js
@@ -1858,7 +1858,7 @@
 	  // from: https://gist.github.com/lorenzopolidori/3794226
 	  var el = document.createElement('div');
 	  el.style[prop] = 'translate3d(1px,1px,1px)';
-	  document.body.insertBefore(el, null);
+	  //document.body.insertBefore(el, null);
 	  var val = getComputedStyle(el).getPropertyValue(map[prop]);
 	  document.body.removeChild(el);
 	  module.exports = null != val && val.length && 'none' != val;
